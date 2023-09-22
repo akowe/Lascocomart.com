132c68f6dac3f54ef9bab609558a7b04254e8e1c
Esther Eze <estherakowe@yahoo.com>
2023-09-22 09:09:06 +0100

    handling 419 error
    after page time out, let it refresh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}">
       <link rel="stylesheet" type="text/css" href="{{ asset('admin/css/adminx.css') }}" media="screen" />
       <!-- CSRF Token -->
       <meta name="csrf-token" content="{{ csrf_token() }}">
