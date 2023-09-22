6400a5004f99b374070abfa1ba44607fffaff382
Esther Eze <estherakowe@yahoo.com>
2023-09-22 12:03:18 +0100

    change message for 419 error

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Page Expired'))
 @section('code', '419')
-@section('message', __('This page was idle. Use the refresh button on your browser or click the button below'))
+@section('message', __('You have left this page inactive. Use refresh button on your browser or click the button below'))
