169199bd3a5f11d254a7fa7c9df1e6b908e887ca
Esther Eze <estherakowe@yahoo.com>
2023-09-22 11:59:21 +0100

    customise error pages

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::layout')
+
+@section('title', __('Page Expired'))
+@section('code', '419')
+@section('message', __('This page was idle. Use the refresh button on your browser or click the button below'))
