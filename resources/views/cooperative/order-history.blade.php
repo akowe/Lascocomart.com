6d4d11348478342e1f92d15d48ef64493850f545
Esther Eze <estherakowe@yahoo.com>
2024-05-14 13:51:32 +0100

    Update order-history.blade.php

--- resources/views/cooperative/order-history.blade.php
+++ resources/views/cooperative/order-history.blade.php
@@ -26,38 +26,23 @@
                               </svg></a>
                         </h2> 
                   </div>
-                  <!-- Page title actions -->
                      <!-- Page title actions -->
                      <div class="col-auto ms-auto d-print-none">
                         <div class="btn-list">
                               <span class="d-none d-sm-inline">
-                                    <a href="#" class="btn">
-                                          Credit: ₦{{ number_format($credit->sum('credit')) }}
-                                    </a>
+                                    <!-- <a href="#" class="btn">
+                                        
+                                    </a> -->
                               </span>
-                              <a href="#" class="btn btn-danger d-none d-sm-inline-block" data-bs-toggle="modal"
+                              <!-- <a href="#" class="btn btn-danger d-none d-sm-inline-block" data-bs-toggle="modal"
                                     data-bs-target="#modal-fund">
-                                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                          viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                          stroke-linecap="round" stroke-linejoin="round">
-                                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                          <path d="M12 5l0 14" />
-                                          <path d="M5 12l14 0" />
-                                    </svg>
-                                    Request credit
-                              </a>
-                              <a href="#" class="btn btn-danger d-sm-none btn-icon" data-bs-toggle="modal"
+                              
+                               
+                              </a> -->
+                              <!-- <a href="#" class="btn btn-danger d-sm-none btn-icon" data-bs-toggle="modal"
                                     data-bs-target="#modal-fund" aria-label="Create new report">
-                                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                          viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                          stroke-linecap="round" stroke-linejoin="round">
-                                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                          <path d="M12 5l0 14" />
-                                          <path d="M5 12l14 0" />
-                                    </svg>
-                              </a>
+                                
+                              </a> -->
                         </div>
                   </div>
 
